8293c88dddde3fac5f72a52075c7c717ce0398a7
iv.popov <ivpopov.sbt@sberbank.ru>
2016-04-27 18:03:21 +0300

    Immutable test example

---
+++ test/immutable_spec.js
@@ -0,0 +1,73 @@
+/* eslint-env node, mocha */
+
+import { expect } from 'chai';
+import { List, Map } from 'immutable';
+
+describe('immutability', () => {
+  describe('a number', () => {
+    function increment(currentState) {
+      return currentState + 1;
+    }
+
+    it('is immutable', () => {
+      const state = 42;
+      const nextState = increment(state);
+
+      expect(nextState).to.equal(43);
+      expect(state).to.equal(42);
+    });
+  });
+
+  // List
+  describe('A List', () => {
+    function addMovie(currentState, movie) {
+      return currentState.push(movie);
+    }
+
+    it('is immutable', () => {
+      const state = List.of('Trainspotting', '28 Days Later');
+      const nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later',
+        'Sunshine'
+      ));
+      expect(state).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later'
+      ));
+    });
+  });
+
+  // Map
+  describe('a tree', () => {
+    function addMovie(currentState, movie) {
+      return currentState.set(
+        'movies',
+        currentState.get('movies').push(movie)
+      );
+    }
+
+    it('is immutable', () => {
+      const state = Map({
+        movies: List.of('Trainspotting', '28 Days Later')
+      });
+      const nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later',
+          'Sunshine'
+        )
+      }));
+      expect(state).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later'
+        )
+      }));
+    });
+  });
+});
